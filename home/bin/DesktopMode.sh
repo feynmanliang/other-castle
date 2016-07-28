5bc195ad645547e79f84a18c5133ce64d0dc4e5b
Feynman Liang <feynman.liang@gmail.com>
2016-07-28 10:45:31 +0100

    Cleans up xmonad and trayer

--- home/bin/DesktopMode.sh
+++ home/bin/DesktopMode.sh
@@ -1,28 +1,12 @@
 #!/bin/sh
 
 # change screen size
-~/.screenlayout/LaptopOnly.sh
-#~/.screenlayout/ViewSonicExternal.sh
+# ~/.screenlayout/LaptopOnly.sh
 
 # i3
 sed -i "9s/\(^.\)/#\1/g
 7s/^#//g" ~/.config/i3/config
 i3-msg restart
-
-# xmonad
-#killall dzen2
-#killall conky
-#sed -i "57s/^-- //g
-#58s/^-- //g
-#61s/\(^.\)/-- \1/g
-#62s/\(^.\)/-- \1/g" ~/.xmonad/xmonad.hs
-#xmonad --recompile
-#xmonad --restart
-
-# trayer
-#killall trayer
-#trayer --edge top --align right --SetDockType true --SetPartialStrut true \
-#  --expand true --width 9 --transparent true --alpha 1 --tint 0x1B1D1E --height 24 &
 
 # key-repeat and capslock<=>esc
 ~/bin/Speedswapper.sh
--- home/bin/LaptopMode.sh
+++ home/bin/LaptopMode.sh
@@ -8,20 +8,5 @@ sed -i "7s/\(^.\)/#\1/g
 9s/^#//g" ~/.config/i3/config
 i3-msg restart
 
-# xmonad
-#killall dzen2
-#killall conky
-#sed -i "57s/\(^.\)/-- \1/g
-#58s/\(^.\)/-- \1/g
-#61s/^-- //g
-#62s/^-- //g" ~/.xmonad/xmonad.hs
-#xmonad --recompile
-#xmonad --restart
-
-# trayer
-#killall trayer
-#trayer --edge top --align right --SetDockType true --SetPartialStrut true \
-#  --expand true --width 9 --transparent true --alpha 1 --tint 0x1B1D1E --height 24 &
-
 # key-repeat and capslock<=>esc
 ~/bin/Speedswapper.sh
