0c62b3609a0b98cd71d5af077b0ffabe0a811073
Feynman Liang <feynman.liang@gmail.com>
2016-08-30 20:23:56 -0700

    Adds lightdm

--- home/bin/lightdm-onstart.sh
+++ home/bin/lightdm-onstart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# You need the "&" unless you want LightDM to be struck here
-compton --config ~/.compton.conf -b
+# You need the "&" unless you want LightDM to be stuck here
+compton --config ~/.compton.conf -b &
