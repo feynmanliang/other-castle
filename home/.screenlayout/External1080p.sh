3ac64f49bab81fa8c6acca7fd2fecc9b6e1d3d3b
Feynman Liang <feynman.liang@gmail.com>
2016-09-18 15:23:54 -0700

    Unbind capslock, set up 4k external

--- home/.screenlayout/External1080p.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-xrandr \
-  --dpi 167 \
-  --output DisplayPort-1 --off \
-  --output DisplayPort-0 --mode 3840x2160 --pos 0x0 --rotate normal --primary \
-  --output eDP --mode 2880x1800 --pos 3840x360 --rotate normal \
-  --output HDMI-0 --off
---
+++ home/.screenlayout/External4k.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DisplayPort-1 --off --output DisplayPort-0 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output eDP --mode 2880x1800 --pos 3840x360 --rotate normal --output HDMI-0 --off
