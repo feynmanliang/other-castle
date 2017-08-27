e0f7e56651faf5b55c377e7b6fece222211cb039
Feynman Liang <feynman.liang@gmail.com>
2017-08-27 16:15:06 -0700

    Updates scripts

--- home/.screenlayout/External1080p.sh
+++ home/.screenlayout/External1080p.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 xrandr \
   --output eDP --mode 2880x1800 \
-  --output HDMI-0 --primary --mode 1920x1080 --scale 1.7x1.7 --panning 3264x1836+2880+0 --right-of eDP \
+  --output HDMI-0 --primary --mode 1920x1080 --scale 1.75x1.75 --panning 3360x1890+2880+0 --right-of eDP \
   --dpi 192
--- home/.screenlayout/External2k.sh
+++ home/.screenlayout/External2k.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 xrandr \
   --output eDP --primary --mode 2880x1800 --pos 0x2160 --scale 1x1 --rotate normal \
-  --output HDMI-0 --off \
   --output DisplayPort-0 \
-  --mode 2560x1440 --pos 208x0 --scale 1.5x1.5 --rotate normal \
+  --output HDMI-0 --mode 2560x1440 --pos 208x0 --scale 1.5x1.5 --rotate normal \
   --dpi 192
