56c054138cb13622b1f83f29747e0060710ed440
Feynman Liang <feynman.liang@gmail.com>
2017-09-17 10:40:07 -0700

    Use home dir

--- home/bin/wallpaper.sh
+++ home/bin/wallpaper.sh
@@ -1,4 +1,4 @@
-#feh --bg-fill '/home/fliang/Dropbox/Pictures (Private)/Desktop Backgrounds/lambda.jpg'
-#feh --bg-fill '/home/fliang/Dropbox/Pictures (Private)/Desktop Backgrounds/wallhaven-200754.jpg'
-feh --bg-fill /home/fliang/.wallpaper.jpg
-# feh --randomize --bg-fill '/home/fliang/Pictures/Pictures (Private)/Desktop Backgrounds/'
+#feh --bg-fill '$HOME/Dropbox/Pictures (Private)/Desktop Backgrounds/lambda.jpg'
+#feh --bg-fill '$HOME/Dropbox/Pictures (Private)/Desktop Backgrounds/wallhaven-200754.jpg'
+feh --bg-fill $HOME/.wallpaper.jpg
+# feh --randomize --bg-fill '$HOME/Pictures/Pictures (Private)/Desktop Backgrounds/'
